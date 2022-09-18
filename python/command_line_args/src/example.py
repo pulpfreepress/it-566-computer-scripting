3d255eb4d9406d0ee4981e732dda288e378b9e0a
swodogrick <rick@pulpfreepress.com>
2022-09-18 10:46:22 -0400

    Sorted out doc comments with pydocstyle.

--- python/command_line_args/src/example.py
+++ python/command_line_args/src/example.py
@@ -1,36 +1,57 @@
-# Class Example
+"""example Module.
 
+Contains one class named Example which defines
+various methods demonstrating this or that.
+"""
+
+"""Example class.
+
+Example class contains various methods that demonstrate various
+topics. Example class will change over time.
+"""
 class Example:
-	"""
-		Doc comments...
-		Example class
+	"""This is a summary line.
+	
+	The summary line starts immediately following the three double quotes.
+	The summary line ends in a period followed by one blank line.
 	"""
 
 	def __init__(self):
+		"""Initialize Example object.
+		
+		Instance variables:
+		- self.count -- Integer 
+		"""
 		self.count = 0
 
 
 	def get_count(self):
+		"""Return value of self.count."""
 		return self.count
 
 
 	def increment_count(self):
+		"""Add 1 to self.count and return."""
 		self.count += 1
 		return self.count 
 
 
 	def sum(self, arg_a, arg_b):
+		"""Add two integer arguments and return the sum."""
 		return arg_a + arg_b
 
 
 	def iter_demo(self):
+		"""Demonstrate interation over characters in a string."""
 		message = 'A string of letters is also a list of chars...'
 		for s in message:
 			print(f'{s} ', end="")
+
 		print()
 
 
 	def lambda_demo(self):
+		"""Demonstate lambda function definition and map() function."""
 		print(f'{list(map(lambda x: x + x , [1,2,3,4,5] ))} ', end="")
 
 
