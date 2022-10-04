dbdd816df1596ec8ca9a971e5f18314a0dff5538
Rick Miller <rick@pulpfreepress.com>
2022-10-03 20:37:55 -0400

    Initial commit

---
+++ python/home_inventory_two/src/main.py
@@ -0,0 +1,10 @@
+"""Explicit main execution module."""
+
+def main():
+	"""Execute main program."""
+	print("Hello World!")
+
+
+# Call main() if this is the main execution module
+if __name__ == '__main__':
+	main()
