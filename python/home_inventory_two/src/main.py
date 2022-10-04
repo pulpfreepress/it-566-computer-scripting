0d09407a5ed6dd94dcadd2f6ce2d888af53be2fd
Rick Miller <rick@pulpfreepress.com>
2022-10-03 21:12:12 -0400

    Minimal Framework

--- python/home_inventory_two/src/main.py
+++ python/home_inventory_two/src/main.py
@@ -1,8 +1,10 @@
 """Explicit main execution module."""
+from inventory_app import InventoryApp
 
 def main():
 	"""Execute main program."""
-	print("Hello World!")
+	inventory_app = InventoryApp()
+	inventory_app.start_program()
 
 
 # Call main() if this is the main execution module
