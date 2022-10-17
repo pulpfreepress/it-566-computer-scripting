9d3682eb75e342dd465408eb6feb74bc4a8eeb01
swodogrick <rick@pulpfreepress.com>
2022-10-17 08:39:15 -0400

    Added drop and create user database scripts. Added statement to create logs directory if it does not already exist.

--- python/sql_test_project/database/initialize_database.sh
+++ python/sql_test_project/database/initialize_database.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# create logs directory if not there
+echo 'Creating logs directory if it does not already exist...'
+mkdir -p logs
+
+echo 'Dropping database...'
 mysql < db_version_1/drop_database.sql 2>&1 > logs/drop_database.log
+echo 'Dropping user...'
+mysql < db_version_1/drop_user.sql 2>&1 > logs/drop_user.log
+echo 'Creating database...'
 mysql < db_version_1/create_database.sql 2>&1 > logs/create_database.log
+echo 'Creating user...'
+mysql < db_version_1/create_user.sql 2>&1 > logs/create_user.log
+echo 'Creating tables...'
 mysql < db_version_1/create_tables.sql 2>&1 > logs/create_tables.log
+echo 'Inserting test data...'
 mysql < db_version_1/insert_test_data.sql 2>&1 > logs/insert_test_data.log
 
 
