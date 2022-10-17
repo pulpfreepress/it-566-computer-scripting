c75e7c8c7298d2ca343a2dd929c3d7b29c46ade2
swodogrick <rick@pulpfreepress.com>
2022-10-17 11:36:04 -0400

    Modified initialize_database.sh script. Added date and stdout and stderr output to both console and log files

--- python/sql_test_project/database/initialize_database.sh
+++ python/sql_test_project/database/initialize_database.sh
@@ -3,19 +3,22 @@
 # create logs directory if not there
 echo 'Creating logs directory if it does not already exist...'
 mkdir -p logs
+echo 'Deleting old log files if they exist...'
+rm -f logs/*
 
-date
-echo 'Dropping database...' | tee -a logs/drop_database.log
-mysql < db_version_1/drop_database.sql | tee -a logs/drop_database.log
-echo 'Dropping user...' 2>&1 >> logs/drop_user.log
-mysql < db_version_1/drop_user.sql 2>&1 >> logs/drop_user.log
-echo 'Creating database...' 2>&1 >> logs/create_database.log
-mysql < db_version_1/create_database.sql 2>&1 > logs/create_database.log
-echo 'Creating user...' 2>&1 >> logs/create_user.log
-mysql < db_version_1/create_user.sql 2>&1 >> logs/create_user.log
-echo 'Creating tables...' 2>&1 >> logs/create_tables.log
-mysql < db_version_1/create_tables.sql 2>&1 >> logs/create_tables.log
-echo 'Inserting test data...' 2>&1 >> logs/insert_test_data.log
-mysql < db_version_1/insert_test_data.sql 2>&1 >> logs/insert_test_data.log
+d=$(date)
+echo $d
+echo  $d': Dropping database...' | tee -a logs/drop_database.log
+mysql < db_version_1/drop_database.sql > >(tee -a logs/drop_database.log) 2> >(tee -a logs/drop_database.log >&2)
+echo $d': Dropping user...' | tee -a logs/drop_user.log
+mysql < db_version_1/drop_user.sql > >(tee -a logs/drop_user.log) 2> >(tee -a logs/drop_user.log >&2)
+echo $d': Creating database...' | tee -a logs/create_database.log
+mysql < db_version_1/create_database.sql > >(tee -a logs/create_database.log) 2> >(tee -a logs/create_database.log >&2)
+echo $d': Creating user...' | tee -a logs/create_user.log
+mysql < db_version_1/create_user.sql > >(tee -a logs/create_user.log) 2> >(tee -a logs/create_user.log >&2)
+echo $d': Creating tables...' | tee -a logs/create_tables.log
+mysql < db_version_1/create_tables.sql > >(tee -a logs/create_tables.log) 2> >(tee -a create_tables.log >&2)
+echo $d': Inserting test data...' | tee -a logs/insert_test_data.log
+mysql < db_version_1/insert_test_data.sql > >(tee -a logs/insert_test_data.log) 2> >(tee -a logs/insert_test_data.log >&2)
 
 
