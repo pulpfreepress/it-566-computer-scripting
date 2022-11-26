95628c3a3915f149e0d1d1319d4b48ae54e4a642
Rick Miller <rick@pulpfreepress.com>
2022-11-26 12:17:55 -0500

    Added the echoserver project

---
+++ python/echoserver/client/client.py
@@ -0,0 +1,30 @@
+import socket
+import json
+
+
+class Client:
+
+	def __init__(self, ip, port):
+		self.ip = ip
+		self.port = port
+		self._connect()
+
+	def _connect(self):
+		print("Connecting to %s:%d" % (self.ip,self.port))
+		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.client.connect((self.ip, self.port))
+		print("Connected to %s:%d" % (self.ip,self.port))
+
+	def _process_server_response(self):
+		response = self.client.recv(4096)
+		json_string = response.decode('utf-8')
+		parsed = json.loads(json_string)
+		print(json.dumps(parsed, indent=4, sort_keys=True))
+
+
+	def send(self, message_string):
+		self.client.send(bytearray(message_string, 'utf-8'))
+		self._process_server_response()
+
+	def close(self):
+		self.client.close()
