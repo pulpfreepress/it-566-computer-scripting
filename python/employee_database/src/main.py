f1ad12c387e3ab207dd75c5542f7e9ca3e0a3c57
Rick Miller <rick@pulpfreepress.com>
2024-06-10 20:34:11 -0400

    Worked employee_database example.

--- python/employee_database/src/main.py
+++ python/employee_database/src/main.py
@@ -1,12 +1,17 @@
 """Main entry point for Employee Database."""
 
 from person import Person
+from employee import Employee
 
 def main()->None:
     """Main entry point."""
     p1 = Person(first_name='Rick', last_name='Miller')
-
     print(f'{p1}')
+
+    e1 = Employee('Rick', 'Miller', '0001', 'Janitor')
+    print(f'{e1}')
+
+    
 
 
 if __name__ == '__main__':
--- python/employee_database/src/person.py
+++ python/employee_database/src/person.py
@@ -7,12 +7,13 @@ class Person:
         self.first_name = first_name
         self.last_name = last_name
 
-    """
+    
     def __str__(self)->str:
         return f'{self.first_name} {self.last_name}'
     
     def __repr__(self)->str:
         return self.__str__()
-    """
+    
+    
 
     
