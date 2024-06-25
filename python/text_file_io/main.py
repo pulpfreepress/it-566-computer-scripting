3b4da58dda823e044fa198ae77f5663d608cd968
Rick Miller <rick@pulpfreepress.com>
2024-06-24 20:10:28 -0400

    Worked file io example

--- python/text_file_io/main.py
+++ python/text_file_io/main.py
@@ -6,9 +6,21 @@ def main():
     greeting = 'Hello World!'
     print(f'Working Dir: {os.getcwd()}')
 
+    working_dir = os.getcwd()
+    data_dir = 'data'
+    file_name = 'greeting.txt'
+    data_dir_path = os.path.join(working_dir, data_dir)
+
+    if not os.path.exists(data_dir_path):
+        os.makedirs(data_dir_path)
+
+
     try:
-        with open('greeting.txt', 'w') as f:
+        with open(os.path.join(data_dir_path, file_name), 'w') as f:
             f.write(greeting)
+            
+
+
 
 
     except (OSError, Exception) as e:
